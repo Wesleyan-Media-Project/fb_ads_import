55f0b7f3f08f2e5e9343773831b263e9a9af68b2
Pavel <poleinikov@wesleyan.edu>
2023-07-31 10:17:29 -0400

    Update backpull2022.R

--- backpull2022.R
+++ backpull2022.R
@@ -13,15 +13,15 @@ DB_CONN = "" ## database connector
 
 QUERY_HASH = data.frame() ## table with hash values for ads
 
-INGESTED_FIELDS = c("id,ad_creation_time,ad_creative_bodies,ad_creative_body",
-                    "ad_creative_link_caption,ad_creative_link_captions",
-                    "ad_creative_link_description,ad_creative_link_descriptions",
-                    "ad_creative_link_titles,ad_creative_link_title",
+INGESTED_FIELDS = c("id,ad_creation_time,ad_creative_bodies",
+                    "ad_creative_link_captions",
+                    "ad_creative_link_descriptions",
+                    "ad_creative_link_titles",
                     "ad_delivery_start_time,ad_delivery_stop_time",
                     "ad_snapshot_url,bylines,currency",
                     "delivery_by_region,demographic_distribution",
-                    "funding_entity,impressions,languages",
-                    "page_id,page_name,potential_reach,publisher_platforms",
+                    "impressions,languages",
+                    "page_id,page_name,publisher_platforms",
                     "spend")
 
 PAGE_BACKPULL_START_DATE = '2021-09-01'
@@ -42,17 +42,10 @@ source("race2022_utils.R")
 a = readLines("tokens.txt")
 a_token = a
 
-# a_token = c("EAADdgbOPVbgBACH96l2C2t2UXCzT1ICtImFx01mS4nDTxQOdWZClr2ZBHW9wK9MzGfhMXfdkcpiLJZBQV2oLXYY1mUnBsE11elhY0UvjZAz6sUJHkWk51KbggIZAaKMlNzfvCDCmctUdhZADQqG3WKJddOqyPkPWEZD",
-#             "EAADdgbOPVbgBABAWd4SlaB2gwy4rU1FXrdjVQnO5ZAi4JdnG1NZBFdRN3TDJCNN87sIJbVQQ9yqgCAXIrM5UzeS5zB3tfZBUc0GCyvUBZAUJFmCewokg0pFtY4vhNRucFac6yOagEdwmXhAe9sTWZAfkI4NXHbPOUOMeRn8ADiYUPd3CeZCmlC5rZC7WjmF7FV7cG3AhpRo0ZC9QwiCyoL0OTi5xUZCqxvuQZD",
-#             "EAADdgbOPVbgBAFSIzYcnk0cbuZBXp5m2EupaLdNo8ZBD1zfrXlAVPvxBkrZBStdXkouUhWfqYs8fisOR97LdW3ZA0fXdZA1jdpYplLNhVPmoMj51XshrakZBKq2ZASI21bwrzfWratZC6ezY5KGBzIF15g9P84qe14oZD",
-#             "EAADdgbOPVbgBAInZCfKtANsT3NP1ZCG3qsvdumV2DEe00uGqT2PRcCNdwUJiG30avC1A3j96fYZB0UdxnmRluc3k2cXZAZCZANpEmrRnPiXcV7qqbRXZA6sIcbaDRAZAUj0JNTH5wSn41Umo9eYNq8ZA6b52tsw1xSO5d4CtWnnZBVWwZDZD")
-
-
-
 ## open a connection to the MySQL server
 DB_CONNECTOR = dbConnect(RMySQL::MySQL(), host="localhost",
                  user="xxxx", password="xxxx",
-                 dbname="textsim_utf8")
+                 dbname="dbase1")
 
 
 ## get the table of search_page_ids
@@ -61,11 +54,11 @@ DB_CONNECTOR = dbConnect(RMySQL::MySQL(), host="localhost",
 ## identify the new page ids and insert them into the page_queue table
 ## get all page ids
 all_page_ids = dbGetQuery(DB_CONNECTOR, 
-                 'select distinct page_id from textsim_new.race2022')
+                 'select distinct page_id from race2022')
 
 ## get the strings with queued page ids
 page_id_queue = dbGetQuery(DB_CONNECTOR,
-                           'select * from textsim_utf8.page_queue')
+                           'select * from page_queue')
 
 ## unpack the strings from the queue
 existing_page_ids = page_id_queue %>% select(x=page_id) %>% 
@@ -96,7 +89,7 @@ if (nrow(new_page_ids) > 0) {
 ## download it and start processing the page_id strings
 ## in chronological order
 page_id_queue = dbGetQuery(DB_CONNECTOR,
-                           'select * from textsim_utf8.page_queue')
+                           'select * from page_queue')
 
 page_id_queue = page_id_queue %>% 
   group_by(page_id) %>% 
@@ -109,7 +102,7 @@ token_num = rep(1:length(a_token), length.out = nrow(page_id_queue)*2)
 q_count = 1
 
 ## this is the endpoint for the ads_archive API
-endpoint_url = 'https://graph.facebook.com/v12.0/ads_archive'
+endpoint_url = 'https://graph.facebook.com/v17.0/ads_archive'
 
 for (i in 1:nrow(page_id_queue)) {
   p_ids = page_id_queue$page_id[i]
@@ -123,7 +116,7 @@ for (i in 1:nrow(page_id_queue)) {
   PERSON = page_id_queue$page_id[i]
   QUERY = page_id_queue$page_id[i]
   
-  query_url = param_set(endpoint_url, key="limit", value="329") %>% 
+  query_url = param_set(endpoint_url, key="limit", value="200") %>% 
     param_set(key="ad_delivery_date_min", min_date) %>% 
     param_set("ad_type", "POLITICAL_AND_ISSUE_ADS") %>% 
     param_set("ad_active_status", "ALL") %>% 
